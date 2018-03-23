93b0e6c1d4fcf88cc0aead731b32938c4fbe3f23
Florian Myter <fmyter@vub.ac.be>
2018-03-23 09:17:46 +0100

    Added main exports

---
+++ captain.ts
@@ -0,0 +1,10 @@
+import {CAPActor} from "./src/CAPActor";
+import {Eventual} from "./src/Eventual";
+import {Consistent} from "./src/Consistent";
+import {Available} from "./src/Available";
+
+export * from "spiders.js"
+export {CAPActor}
+export {Eventual}
+export {Consistent}
+export {Available}
